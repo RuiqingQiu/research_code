a3dc33f5d6ba9ba5fffddf2fc2e7890341430aaa
Ruiqing Qiu <rqiu@tscc-login2.sdsc.edu>
2017-04-25 11:37:22 -0700

    getting 1000 examples from each class

---
+++ py/cifar_subset_10000_train.py
@@ -0,0 +1,24 @@
+# File path for the whole cifar dataset
+cifar_whole_data_path = '/oasis/tscc/scratch/rqiu/cifar_train_c.txt'
+cifar_whole_label_path = '../../cifar_train_label.txt'
+
+f_data = open('/oasis/tscc/scratch/rqiu/cifar_10000_data.txt','w')
+f_label = open('/oasis/tscc/scratch/rqiu/cifar_10000_label.txt','w')
+label = []
+with open(cifar_whole_label_path) as label_file:
+    for line in label_file:
+        label.append(int(line.split()[0]))
+class_count = [0] * 10
+count = 0
+with open(cifar_whole_data_path) as data_file:
+    for line in data_file:
+        if class_count[label[count]] < 1000:
+            f_data.write(line)
+            f_label.write(str(label[count]) + '\n')
+            class_count[label[count]] += 1
+        count += 1
+
+print class_count
+
+
+
